68a48950eaf5160bc184750f1f9803f1db832ebd
sergio carrillo <dieler.tk.s@gmail.com>
2025-05-16 19:55:43 -0500

    feat: refactor build.gradle files to use root project properties for SDK versions and dependencies; improve project structure and maintainability

--- presentation/technician/build.gradle.kts
+++ presentation/technician/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
 
 android {
     namespace = "com.dscorp.technician"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 26
+        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -23,21 +24,27 @@ android {
             )
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    // AndroidX Core
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    
+    // Material Design
+    implementation("com.google.android.material:material:1.12.0")
+    
+    // Test dependencies
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
