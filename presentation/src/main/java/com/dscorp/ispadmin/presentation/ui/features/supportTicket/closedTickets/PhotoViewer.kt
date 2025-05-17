01d8964dee6dcdb2373b0ce50c2a18b6365cea93
sergio carrillo <dieler.tk.s@gmail.com>
2025-05-16 20:38:22 -0500

    feat: update Gradle and plugin versions to 8.10.0; improve project compatibility and performance

--- presentation/src/main/java/com/dscorp/ispadmin/presentation/ui/features/supportTicket/closedTickets/PhotoViewer.kt
+++ presentation/src/main/java/com/dscorp/ispadmin/presentation/ui/features/supportTicket/closedTickets/PhotoViewer.kt
@@ -3,12 +3,8 @@ package com.dscorp.ispadmin.presentation.ui.features.supportTicket.closedTickets
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dscorp.ispadmin.R
 import com.dscorp.ispadmin.databinding.ActivityPhotoViewerBinding
 import com.dscorp.ispadmin.presentation.ui.features.composecomponents.ZoomableImage
-import com.dscorp.ispadmin.presentation.ui.features.supportTicket.SupportTicketHelper
 import com.example.data2.data.response.AssistanceTicketResponse
 
 class PhotoViewer : AppCompatActivity() {
